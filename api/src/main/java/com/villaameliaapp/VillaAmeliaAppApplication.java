345c432f41033d8f43db578f74bfcaee6ea29031
Juan Pablo Castaño Tinoco <jp.tinoco@transportsystems.co>
2022-06-26 20:45:47 -0500

    1 Init API project

---
+++ api/src/main/java/com/villaameliaapp/VillaAmeliaAppApplication.java
@@ -0,0 +1,13 @@
+package com.villaameliaapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VillaAmeliaAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VillaAmeliaAppApplication.class, args);
+    }
+
+}
---
+++ api/src/test/java/com/villaameliaapp/VillaAmeliaAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.villaameliaapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VillaAmeliaAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
