53667caf5de4d245730f0edefa7dfc14db619cfa
js.mejia <js.mejia@transportsystems.co>
2022-11-08 22:35:57 -0500

    feature/51 Ajustes

--- api/src/main/java/com/itender/api/rest/BenchController.java
+++ api/src/main/java/com/itender/api/rest/BenchController.java
@@ -6,13 +6,12 @@ import javax.validation.constraints.NotNull;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -50,7 +49,7 @@ public class BenchController {
             }
     )
     @PostMapping
-    public ResponseEntity<UUID> createBench(@NotNull @ModelAttribute BenchRequest request)
+    public ResponseEntity<UUID> createBench(@NotNull @RequestBody BenchRequest request)
             throws BenchException {
         return new ResponseEntity<>(benchService.createBench(request), HttpStatus.CREATED);
     }
