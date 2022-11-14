317035003898e651d8c1a80028b393ede5162da2
Juan Pablo Castaño <jp.tinoco@transportsystems.co>
2022-11-14 16:43:37 -0500

    Fix routing

--- front/src/app/app-routing.module.ts
+++ front/src/app/app-routing.module.ts
@@ -3,7 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home/home.component';
 
 const routes: Routes = [
-  {path:'home/:id',
+  {path:'home',
 component: HomeComponent}
 ];
 
